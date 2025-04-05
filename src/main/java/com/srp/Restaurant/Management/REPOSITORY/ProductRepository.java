0c390f27363c726aa142153c5044c7d7c98e7b76
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-06 00:41:42 +0530

    Updated features and fixed bugs

--- src/main/java/com/srp/Restaurant/Management/REPOSITORY/ProductRepository.java
+++ src/main/java/com/srp/Restaurant/Management/REPOSITORY/ProductRepository.java
@@ -1,14 +1,15 @@
 package com.srp.Restaurant.Management.REPOSITORY;
 
-import com.srp.Restaurant.Management.DTO.ProductDTO;
 import com.srp.Restaurant.Management.ENTITIES.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
     List<Product> findAllByCategoryId(Long categoryId);
+
     List<Product> findAllByCategoryIdAndNameContaining(Long categoryId, String title);
 
     List<Product> findAllByCategoryIdAndNameContainingIgnoreCase(Long categoryId, String title);
+
 }
