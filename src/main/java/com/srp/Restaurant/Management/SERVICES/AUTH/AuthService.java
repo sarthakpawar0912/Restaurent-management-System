c7663d32e67308506cc4432636dc2324eaa0d578
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-06 00:48:09 +0530

    Updated features and fixed bugs

--- src/main/java/com/srp/Restaurant/Management/SERVICES/AUTH/AuthService.java
+++ src/main/java/com/srp/Restaurant/Management/SERVICES/AUTH/AuthService.java
@@ -3,7 +3,6 @@ package com.srp.Restaurant.Management.SERVICES.AUTH;
 import com.srp.Restaurant.Management.DTO.SignUpRequest;
 import com.srp.Restaurant.Management.DTO.UserDTO;
 
-
 public interface AuthService {
 
     UserDTO createUser(SignUpRequest signUpRequest);
