7a64c5bda64103df7c81bd462bd3fce8f453b868
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-06 00:37:01 +0530

    Updated features and fixed bugs

--- src/main/java/com/srp/Restaurant/Management/ENTITIES/User.java
+++ src/main/java/com/srp/Restaurant/Management/ENTITIES/User.java
@@ -64,50 +64,5 @@ public class User implements UserDetails {
         return true;
     }
 
-    public User(Long id, String name, String email, String password, UserRole userRole) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.userRole = userRole;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UserRole getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
-
-    public User() {
-    }
 }
