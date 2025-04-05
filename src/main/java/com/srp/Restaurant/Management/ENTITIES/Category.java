3518446b836b32bdfe12fe1b8f3ed37cb05a8da4
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-06 00:35:12 +0530

    Updated features and fixed bugs

--- src/main/java/com/srp/Restaurant/Management/ENTITIES/Category.java
+++ src/main/java/com/srp/Restaurant/Management/ENTITIES/Category.java
@@ -21,10 +21,9 @@ public class Category {
     @Column(columnDefinition = "longblob")
     private byte[] img;
 
-
-
     public CategoryDTO getCategoryDTO(){
         CategoryDTO categoryDTO= new CategoryDTO();
+
         categoryDTO.setId(id);
         categoryDTO.setName(name);
         categoryDTO.setDescription(description);
@@ -33,50 +32,4 @@ public class Category {
         return categoryDTO;
     }
 
-
-
-
-
-
-    public Category(Long id, String name, String description, byte[] img) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.img = img;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public byte[] getImg() {
-        return img;
-    }
-
-    public void setImg(byte[] img) {
-        this.img = img;
-    }
-
-    public Category() {
-    }
 }
